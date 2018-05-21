ca62b8a6d829afaa61bcb69a32843571aa95db50
CrispimPaiano <crispimpds5@gmail.com>
2018-05-21 09:56:30 -0300

    Inicialização do repositório

---
+++ ExemploCineminha/src/Principal.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Crispim Paiano dos Santos
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Minha mensagem");
+    }
+    
+}
